e6f4fae6988c7d67c842470ac3811035ff4c2ce1
TheWilli67 <william.hertrich@numericable.fr>
2022-05-04 11:34:59 +0200

    session de 1h30
    ajout de l'affichage de l'ajout (formulaire) + affichetout.html + affichage de détails dans l'affichage individuel + main.html + modif dans url.py + modifs dans views.py

--- application/urls.py
+++ application/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('index/', views.index),
     path('formulaire/', views.formulaire),
     path('affichage/', views.affichage),
+    path('affichetout/', views.affichetout),
+    path('main/', views.main),
+    path('traitement/', views.traitement),
+    path('ajout/', views.ajout),
+    path('affichage/<int:id>/',views.affichage),
+    path('update/<int:id>/',views.update),
 ]
--- application/views.py
+++ application/views.py
@@ -1,16 +1,16 @@
-from .forms import LivreForm
+from .form import LivreForm
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 from . import models
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-# Create your views here.
 
 def index(request):
     return render(request, 'application/index.html')
 
-
+def main(request):
+    return render(request, 'main.html')
 
 def formulaire(request):
     if request.method == "POST":
@@ -24,10 +24,42 @@ def formulaire(request):
             return render(request, 'application/formulaire.html', {'form': form})
 
 
-def affichage(request):
+def traitement(request):
     pForm = LivreForm(request.POST)
     if pForm.is_valid():
         livre = pForm.save()
         return render(request, 'application/affichage.html' , {'livre' : livre})
     else:
         return render(request, 'application/formulaire.html', {'form': pForm})
+    
+def affichage(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    return render(request,"application/affichage.html",{"livre": livre})
+
+def affichetout(request):
+    livre = models.Livre.objects.all()
+    return render(request, 'application/affichetout.html', {"livre": livre})
+
+
+def ajout(request):
+    if request.method == "POST":
+        form = LivreForm(request)
+        if form.is_valid(): # validation du formulaire.
+            livre = form.save() # sauvegarde dans la base
+            return render(request,"/application/affichage.html",{"livre" : livre})
+        else:
+            return render(request,"application/ajout.html",{"form": form})
+    else :
+        form = LivreForm() # création d'un formulaire vide
+        return render(request,"application/ajout.html",{"form" : form})
+    
+    
+def update(request, id):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        livre = lform.save(commit=False)
+        livre.id = id; # modification de l'id de l'objet
+        livre.save() # mise à jour dans la base puisque l'id du livre existe déja.
+        return HttpResponseRedirect("application/")
+    else:
+        return render(request, "application/update.html", {"form": lform, "id": id})
